005f0b5ecbd9cd51bd11bca6fb68124d428f3179
Neb Seniah <89611351+Neb-Seniah@users.noreply.github.com>
2024-01-31 10:32:22 -0600

    Colours
    Changed the colours of the hitboxes for identification, corrected which regions the states belong to.

--- Assets/Scripts/TerritoryManager.cs
+++ Assets/Scripts/TerritoryManager.cs
@@ -38,42 +38,42 @@ public class TerritoryManager : MonoBehaviour
 
 			if(countHandler.territory.region == Territory.Regions.Oceania)
 			{
-				countHandler.TintColor(new Color32(56,218,56,255));
+				countHandler.TintColor(new Color32(165,234,202,255));
 			}
 
 			if(countHandler.territory.region == Territory.Regions.Asia)
 			{
-				countHandler.TintColor(new Color32(56,218,56,255));
+				countHandler.TintColor(new Color32(220,176,192,255));
 			}
 
 			if(countHandler.territory.region == Territory.Regions.Europe)
 			{
-				countHandler.TintColor(new Color32(56,218,56,255));
+				countHandler.TintColor(new Color32(229,172,74,255));
 			}
 
 			if(countHandler.territory.region == Territory.Regions.Canada)
 			{
-				countHandler.TintColor(new Color32(56,218,56,255));
+				countHandler.TintColor(new Color32(209,124,124,255));
 			}
 			
 			if(countHandler.territory.region == Territory.Regions.UnitedStates)
 			{
-				countHandler.TintColor(new Color32(56,218,56,255));
+				countHandler.TintColor(new Color32(91,136,214,255));
 			}
 
 			if(countHandler.territory.region == Territory.Regions.SouthAmerica)
 			{
-				countHandler.TintColor(new Color32(56,218,56,255));
+				countHandler.TintColor(new Color32(108,208,90,255));
 			}
 
 			if(countHandler.territory.region == Territory.Regions.NorthAfrica)
 			{
-				countHandler.TintColor(new Color32(56,218,56,255));
+				countHandler.TintColor(new Color32(180,115,205,255));
 			}
 
 			if(countHandler.territory.region == Territory.Regions.SouthAfrica)
 			{
-				countHandler.TintColor(new Color32(56,218,56,255));
+				countHandler.TintColor(new Color32(206,73,157,255));
 			}
 		}
 	}
