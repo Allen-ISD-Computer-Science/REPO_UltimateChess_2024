f2f2a53708151d4de38982bc0956235a7dc7d374
TahmidR2456 <111006411+TahmidR2456@users.noreply.github.com>
2024-03-01 14:59:31 -0600

    preliminary settings build

--- Assets/Scripts/mainmenu.cs
+++ Assets/Scripts/mainmenu.cs
@@ -5,8 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public float delayBeforeLoading = 1.0f; // Adjust the delay time as needed
+
     public void LoadScene(string sceneName)
     {
-    SceneManager.LoadScene(sceneName);
+        StartCoroutine(LoadSceneAfterDelay(sceneName));
+    }
+
+    IEnumerator LoadSceneAfterDelay(string sceneName)
+    {
+        yield return new WaitForSeconds(delayBeforeLoading);
+        SceneManager.LoadScene(sceneName);
     }
 }
