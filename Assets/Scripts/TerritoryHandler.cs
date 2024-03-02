1673ff5a989717fd1c9c78b15b31268043862d91
Neb Seniah <89611351+Neb-Seniah@users.noreply.github.com>
2024-03-01 19:22:48 -0600

    Worked on Colours
    Fixed the camera as it was bugged so some of the territories would be the wrong colour, it was originally fixed by kevin but he didn't commit it.
    
    Added the hover and click colour changes.
    
    Dont know what is up with it adding two files, since I didn't
    
    In conjunction with issues 63,64,66.

--- Assets/Scripts/TerritoryHandler.cs
+++ Assets/Scripts/TerritoryHandler.cs
@@ -9,27 +9,44 @@ public class TerritoryHandler : MonoBehaviour {
 	public Territory territory;
 
 	private SpriteRenderer sprite;
-	
+
 	public Color32 oldColor;
 	public Color32 startColor;
 	public Color32 hoverColor;
 
 	void Awake()
-	{ 
+	{
 		sprite = GetComponent<SpriteRenderer>();
 		sprite.color = startColor;
 	}
-	
-	void OnMouseEnter(){
+
+	void OnMouseEnter() {
 		oldColor = sprite.color;
-		sprite.color = hoverColor;
+		sprite.color = new Color32((byte)((int)oldColor.r + 10), (byte)((int)oldColor.g + 10), (byte)((int)oldColor.b + 10), 255);
 	}
 
-	void OnMouseExit(){
-		sprite.color = oldColor;
+	void OnMouseExit() {
+		if (sprite.color != new Color32((byte)((int)oldColor.r + 20), (byte)((int)oldColor.g + 20), (byte)((int)oldColor.b + 20), 255))
+		{
+			sprite.color = oldColor;
+		}
 	}
 
-	void OnDrawGizmos()
+	private void OnMouseDown() {
+		sprite.color = new Color32((byte)((int)oldColor.r + 20), (byte)((int)oldColor.g + 20), (byte)((int)oldColor.b + 20), 255);
+	}
+
+	private void OnMouseUp()
+	{
+        // There needs to be a check here for if the mouse is still hovering over the territory or not, since if its not it needs to turn to oldcolor instead of oldcolor + 10.
+        if (sprite.color != oldColor)
+		{
+			sprite.color = new Color32((byte)((int)oldColor.r + 10), (byte)((int)oldColor.g + 10), (byte)((int)oldColor.b + 10), 255);
+		}
+
+	}
+
+    void OnDrawGizmos()
 	{
 		territory.name = name;
 		this.tag = "Territory";
