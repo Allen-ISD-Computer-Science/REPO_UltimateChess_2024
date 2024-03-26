ceac5fc99f83e12b125363ed392e186b196b2ca5
spectreseldom <hutchingskids03@gmail.com>
2024-03-26 06:19:42 -0500

    Fixed bug that was causing most issues, including sprite error

--- Assets/Assets of my assets/Chess/Scripts/Chessman.cs
+++ Assets/Assets of my assets/Chess/Scripts/Chessman.cs
@@ -4,56 +4,58 @@ using UnityEngine;
 
 public class Chessman : MonoBehaviour
 {
-    // References
     public GameObject controller;
     public GameObject movePlate;
 
-    // Positions
     private int xBoard = -1;
     private int yBoard = -1;
 
-    // Variable, keep track of black or white
     private string player;
 
-    // References for all sprites
     public Sprite black_queen, black_knight, black_bishop, black_king, black_rook, black_pawn;
     public Sprite white_queen, white_knight, white_bishop, white_king, white_rook, white_pawn;
 
     public void Activate()
     {
+        //Get the game controller
         controller = GameObject.FindGameObjectWithTag("GameController");
 
-        // Take instantiated location and adjust transform
+        //Take the instantiated location and adjust transform
         SetCoords();
 
+        //Choose correct sprite based on piece's name
         switch (this.name)
         {
-            case "black queen": this.GetComponent<SpriteRenderer>().sprite = black_queen; break;
-            case "black knight": this.GetComponent<SpriteRenderer>().sprite = black_knight; break;
-            case "black king": this.GetComponent<SpriteRenderer>().sprite = black_king; break;
-            case "black rook": this.GetComponent<SpriteRenderer>().sprite = black_rook; break;
-            case "black pawn": this.GetComponent<SpriteRenderer>().sprite = black_pawn; break;
-
-            case "white queen": this.GetComponent<SpriteRenderer>().sprite = white_queen; break;
-            case "white knight": this.GetComponent<SpriteRenderer>().sprite = white_knight; break;
-            case "white king": this.GetComponent<SpriteRenderer>().sprite = white_king; break;
-            case "white rook": this.GetComponent<SpriteRenderer>().sprite = white_rook; break;
-            case "white pawn": this.GetComponent<SpriteRenderer>().sprite = white_pawn; break;
-
+            case "black_queen": this.GetComponent<SpriteRenderer>().sprite = black_queen; player = "black"; break;
+            case "black_knight": this.GetComponent<SpriteRenderer>().sprite = black_knight; player = "black"; break;
+            case "black_bishop": this.GetComponent<SpriteRenderer>().sprite = black_bishop; player = "black"; break;
+            case "black_king": this.GetComponent<SpriteRenderer>().sprite = black_king; player = "black"; break;
+            case "black_rook": this.GetComponent<SpriteRenderer>().sprite = black_rook; player = "black"; break;
+            case "black_pawn": this.GetComponent<SpriteRenderer>().sprite = black_pawn; player = "black"; break;
+            case "white_queen": this.GetComponent<SpriteRenderer>().sprite = white_queen; player = "white"; break;
+            case "white_knight": this.GetComponent<SpriteRenderer>().sprite = white_knight; player = "white"; break;
+            case "white_bishop": this.GetComponent<SpriteRenderer>().sprite = white_bishop; player = "white"; break;
+            case "white_king": this.GetComponent<SpriteRenderer>().sprite = white_king; player = "white"; break;
+            case "white_rook": this.GetComponent<SpriteRenderer>().sprite = white_rook; player = "white"; break;
+            case "white_pawn": this.GetComponent<SpriteRenderer>().sprite = white_pawn; player = "white"; break;
         }
     }
 
     public void SetCoords()
     {
+        //Get the board value in order to convert to xy coords
         float x = xBoard;
         float y = yBoard;
 
+        //Adjust by variable offset
         x *= 0.66f;
         y *= 0.66f;
 
+        //Add constants (pos 0,0)
         x += -2.3f;
         y += -2.3f;
 
+        //Set actual unity values
         this.transform.position = new Vector3(x, y, -1.0f);
     }
 
@@ -76,4 +78,5 @@ public class Chessman : MonoBehaviour
     {
         yBoard = y;
     }
+
 }
--- Assets/Assets of my assets/Chess/Scripts/Game.cs
+++ Assets/Assets of my assets/Chess/Scripts/Game.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+
     public GameObject chesspiece;
 
     private GameObject[,] positions = new GameObject[8, 8];
@@ -15,9 +16,8 @@ public class Game : MonoBehaviour
     private bool gameOver = false;
 
 
-
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
         playerWhite = new GameObject[] { Create("white_rook", 0, 0), Create("white_knight", 1, 0),
             Create("white_bishop", 2, 0), Create("white_queen", 3, 0), Create("white_king", 4, 0),
@@ -25,15 +25,14 @@ public class Game : MonoBehaviour
             Create("white_pawn", 0, 1), Create("white_pawn", 1, 1), Create("white_pawn", 2, 1),
             Create("white_pawn", 3, 1), Create("white_pawn", 4, 1), Create("white_pawn", 5, 1),
             Create("white_pawn", 6, 1), Create("white_pawn", 7, 1) };
-        
-        playerBlack = new GameObject[] { Create("black_rook", 0, 7), Create("black_knight", 1, 7),
-            Create("black_bishop", 2, 7), Create("black_queen", 3, 7), Create("black_king", 4, 7),
-            Create("black_bishop", 5, 7), Create("black_knight", 6, 7), Create("black_rook", 7, 7),
+        playerBlack = new GameObject[] { Create("black_rook", 0, 7), Create("black_knight",1,7),
+            Create("black_bishop",2,7), Create("black_queen",3,7), Create("black_king",4,7),
+            Create("black_bishop",5,7), Create("black_knight",6,7), Create("black_rook",7,7),
             Create("black_pawn", 0, 6), Create("black_pawn", 1, 6), Create("black_pawn", 2, 6),
             Create("black_pawn", 3, 6), Create("black_pawn", 4, 6), Create("black_pawn", 5, 6),
             Create("black_pawn", 6, 6), Create("black_pawn", 7, 6) };
 
-        // Set all positions on position board
+        //Set all piece positions on the positions board
         for (int i = 0; i < playerBlack.Length; i++)
         {
             SetPosition(playerBlack[i]);
@@ -44,11 +43,11 @@ public class Game : MonoBehaviour
     public GameObject Create(string name, int x, int y)
     {
         GameObject obj = Instantiate(chesspiece, new Vector3(0, 0, -1), Quaternion.identity);
-        Chessman cm = obj.GetComponent<Chessman>();
-        cm.name = name;
+        Chessman cm = obj.GetComponent<Chessman>(); 
+        cm.name = name; //This is a built in variable that Unity has, so we did not have to declare it before
         cm.SetXBoard(x);
         cm.SetYBoard(y);
-        cm.Activate();
+        cm.Activate(); //It has everything set up so it can now Activate()
         return obj;
     }
 
@@ -56,6 +55,24 @@ public class Game : MonoBehaviour
     {
         Chessman cm = obj.GetComponent<Chessman>();
 
+        //Overwrites either empty space or whatever was there
         positions[cm.GetXBoard(), cm.GetYBoard()] = obj;
     }
+
+    public void SetPositionEmpty(int x, int y)
+    {
+        positions[x, y] = null;
+    }
+
+    public GameObject GetPosition(int x, int y)
+    {
+        return positions[x, y];
+    }
+
+    public bool PositionOnBoard(int x, int y)
+    {
+        if (x < 0 || y < 0 || x >= positions.GetLength(0) || y >= positions.GetLength(1)) return false;
+        return true;
+    }
+
 }
