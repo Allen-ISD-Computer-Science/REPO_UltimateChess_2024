0e9e426abf6a5a8d3c25c1d133e29f4928f05765
IsaacDaKing <isaac.pandyan@student.allenisd.org>
2024-03-26 00:55:21 -0500

    Revert "Put the moveplate, base of the moving function and rules"
    This reverts commit 27c1182d03e0d0fea70d3eb874167925b65969c2.

--- Assets/Assets of my assets/Chess/Scripts/Game.cs
+++ Assets/Assets of my assets/Chess/Scripts/Game.cs
@@ -6,7 +6,6 @@ public class Game : MonoBehaviour
 {
     public GameObject chesspiece;
 
-    //Positions set for all pieces
     private GameObject[,] positions = new GameObject[8, 8];
     private GameObject[] playerBlack = new GameObject[16];
     private GameObject[] playerWhite = new GameObject[16];
@@ -58,21 +57,5 @@ public class Game : MonoBehaviour
         Chessman cm = obj.GetComponent<Chessman>();
 
         positions[cm.GetXBoard(), cm.GetYBoard()] = obj;
-    }
-
-    public void SetPositionEmpty(int x, int y)
-    {
-        positions[x, y] = null;
-    }
-
-    public GameObject GetPosition(int x, int y)
-    {
-        return positions[x, y];
-    }
-
-    public bool PositionOnBoard(int x, int y)
-    {
-        if (x < 0 || y < 0 || x >= positions.GetLength(0) || y >= positions.GetLength(1)) return false;
-        return true;
     }
 }
